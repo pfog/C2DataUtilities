e624e1e52a3c81925df623513d30b5f606c5ba64
Jesse T Holzer <jesse.holzer@pnnl.gov>
2020-09-07 01:07:05 -0700

    swsh_utils.py holds multiple implementations

---
+++ py/swsh_utils.py
@@ -0,0 +1,9 @@
+#
+# Grid Optimization Competion
+# author: Jesse Holzer
+# date: 2020-09-06
+#
+
+from swsh_utils_cy import solve_cy
+# import other implementations of swsh_utils here
+# to make them all available from swsh_utils
